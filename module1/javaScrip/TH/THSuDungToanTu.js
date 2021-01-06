efab1644824e62611a10f1ef764eba75a90d319c
thangdinh1607 <thangdinh1607@gmail.com>
2021-01-06 11:38:20 +0700

    upbt

---
+++ module1/javaScrip/TH/THSuDungToanTu.js
@@ -0,0 +1,16 @@
+
+// NHập vào chiều rộng
+let inputWidth;
+    inputWidth=prompt("nhập vào width :");
+let width = parseInt(inputWidth);
+
+
+// nhập vào chiều cao
+let inputHeight;
+    inputHeight=prompt("NHập vào chiều cao");
+let height = parseInt(inputHeight);
+
+
+//diện tích hình chữ nhât
+let  acreageRectangle = width*height;
+document.write("Diện tích hình chữ nhật là : "  + acreageRectangle);
